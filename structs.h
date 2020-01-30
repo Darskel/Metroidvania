5f011e9619d7227d5af826b93ee8a712b450ca50
Dems <tom.didier4@gmail.com>
2020-01-30 14:41:48 +0100

    Pause changé pour Linux

--- structs.h
+++ structs.h
@@ -3,7 +3,7 @@
     #define PAUSE system("PAUSE");
     #define CLEAR system("CLS");
 #elif defined(__unix)
-    #define PAUSE system("pause");
+    #define PAUSE system("read -rsp $'Press any key to continue...\n' -n 1");
     #define CLEAR system("clear");
 #endif
 
